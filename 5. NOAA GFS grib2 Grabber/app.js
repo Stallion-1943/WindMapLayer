59ad0b853f787899c4d977d98534c979e5d962df
Sehoon Jeong <tearfield@naver.com>
2016-09-29 02:10:04 +0000

    NOAA NOMADS 접근

--- 5. NOAA GFS grib2 Grabber/app.js
+++ 5. NOAA GFS grib2 Grabber/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var path = require('path');
+var fs = require("fs");
 
 var async = require('async');
 var socketio = require('socket.io');
@@ -14,9 +15,16 @@ var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
-router.use(express.static(path.resolve(__dirname, 'public')));
+var public_dir = path.resolve(__dirname, "public");
+
+var grib2 = fs.createWriteStream(path.join(path.resolve(public_dir, "grib2"), "/gfs.2016092818.grib2"));
+var request_to_nomads = http.get("http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t18z.pgrb2.1p00.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.2016092818", function(response) {
+  response.pipe(grib2);
+});
+
+router.use(express.static(public_dir));
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+  console.log("WindMapLayer server listening at", addr.address + ":" + addr.port);
 });
