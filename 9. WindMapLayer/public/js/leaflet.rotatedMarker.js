03fc5ec5204fd5e85b67f17f731f875b030e0906
정세훈 <tearfield@sk.com>
2016-10-12 10:31:42 +0900

    beautify 적용

--- 9. WindMapLayer/public/js/leaflet.rotatedMarker.js
+++ 9. WindMapLayer/public/js/leaflet.rotatedMarker.js
@@ -5,13 +5,13 @@
 
     var oldIE = (L.DomUtil.TRANSFORM === 'msTransform');
 
-    L.Marker.addInitHook(function () {
+    L.Marker.addInitHook(function() {
         var iconOptions = this.options.icon && this.options.icon.options;
         var iconAnchor = iconOptions && this.options.icon.options.iconAnchor;
         if (iconAnchor) {
             iconAnchor = (iconAnchor[0] + 'px ' + iconAnchor[1] + 'px');
         }
-        this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom' ;
+        this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom';
         this.options.rotationAngle = this.options.rotationAngle || 0;
     });
 
@@ -20,13 +20,13 @@
             proto_initIcon.call(this);
         },
 
-        _setPos: function (pos) {
+        _setPos: function(pos) {
             proto_setPos.call(this, pos);
 
-            if(this.options.rotationAngle) {
-                this._icon.style[L.DomUtil.TRANSFORM+'Origin'] = this.options.rotationOrigin;
+            if (this.options.rotationAngle) {
+                this._icon.style[L.DomUtil.TRANSFORM + 'Origin'] = this.options.rotationOrigin;
 
-                if(oldIE) {
+                if (oldIE) {
                     // for IE 9, use the 2D rotation
                     this._icon.style[L.DomUtil.TRANSFORM] = 'rotate(' + this.options.rotationAngle + 'deg)';
                 } else {
