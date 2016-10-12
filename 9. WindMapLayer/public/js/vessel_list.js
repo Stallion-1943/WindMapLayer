9eea0ed30d2defe2a4e052d45196f0f0297e86b7
Sehoon Jeong <tearfield@naver.com>
2016-10-12 01:08:32 +0000

    선박 마커 추가 완료

---
+++ 9. WindMapLayer/public/js/vessel_list.js
@@ -0,0 +1,11 @@
+var vessel_list = [
+    [2.28455, 124.80469, 10, "YK SOVEREIGN<br>Distance: 390km<br>Speed: 16.25knot<br>RPM: 70.60rpm"],
+    [12.03932, 88.94531, 40, "SK SUMMIT<br>Distance: 51km<br>Speed: 17.33knot<br>RPM: 105.52rpm"],
+    [16.97274, 64.16016, 80, "SK SUPREME<br>Distance: 96km<br>Speed: 19.12knot<br>RPM: 113.90rpm"],
+    [32.54681, 141.06445, 150, "SK SPLENDOR<br>Distance: 0km<br>Speed: 13.54knot<br>RPM: 114.10rpm"],
+    [16.63619, 197.75391, 220, "SK STELLAR<br>Distance: 72km<br>Speed: 11.00knot<br>RPM: 98.90rpm"],
+    [34.59704, 345.23438, 0, "G. COMMANDER<br>Distance: 319km<br>Speed: 17.11knot<br>RPM: 92.30rpm"],
+    [41.77131, 233.96484, 45, "G. FOREVER<br>Distance: 294km<br>Speed: 13.65knot<br>RPM: 69.90rpm"],
+    [22.26876, 158.64258, 75, "G. SWAN<br>Distance: 140km<br>Speed: 19.11knot<br>RPM: 41.30rpm"],
+    [46.31658, 153.45703, 210, "G. PARAGON<br>Distance: 240km<br>Speed: 15.15knot<br>RPM: 98.76rpm"]
+];
